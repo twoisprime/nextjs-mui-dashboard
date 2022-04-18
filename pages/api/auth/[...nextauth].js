3220584f78e385e5c1ecf306f36ce8bc08232922
Sebastian Bas <sbaskana@gmail.com>
2022-04-18 21:51:38 +0200

    revoke session when token expires

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import FormData from 'form-data'
+import { signOut } from "next-auth/react"
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
@@ -55,9 +56,18 @@ export default NextAuth({
       // assign the accessToken to the `token` object, so it will be available on the `session` callback
       if (user) {
         token.accessToken = user.access_token
+        token.accessTokenExpires = Date.now() + parseInt(user.access_token_expires) * 1000  // milliseconds
       }
-      // console.log(user)
-      // console.log(token)
+
+      // TODO
+      // Return previous token if the access token has not expired yet
+      // if (Date.now() < token.accessTokenExpires) {
+      //   return token
+      // }
+
+      // Access token has expired, try to update it
+      // return refreshAccessToken(token)
+
       return token
     },
 
@@ -66,6 +76,10 @@ export default NextAuth({
       // Assign the accessToken to the `session` object, so it will be available on our app through `useSession` hooks
       if (token) {
         session.accessToken = token.accessToken
+      }
+      // check if access token expired
+      if (token && Date.now() > token.accessTokenExpires) {
+        return {}  // no session -> require login
       }
       return session
     }
@@ -78,3 +92,50 @@ export default NextAuth({
     // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
   }
 })
+
+// TODO
+/**
+ * https://next-auth.js.org/tutorials/refresh-token-rotation
+ * Takes a token, and returns a new token with updated
+ * `accessToken` and `accessTokenExpires`. If an error occurs,
+ * returns the old token and an error property
+ */
+//  async function refreshAccessToken(token) {
+//   try {
+//     const url =
+//       "https://oauth2.googleapis.com/token?" +
+//       new URLSearchParams({
+//         client_id: process.env.GOOGLE_CLIENT_ID,
+//         client_secret: process.env.GOOGLE_CLIENT_SECRET,
+//         grant_type: "refresh_token",
+//         refresh_token: token.refreshToken,
+//       })
+
+//     const response = await fetch(url, {
+//       headers: {
+//         "Content-Type": "application/x-www-form-urlencoded",
+//       },
+//       method: "POST",
+//     })
+
+//     const refreshedTokens = await response.json()
+
+//     if (!response.ok) {
+//       throw refreshedTokens
+//     }
+
+//     return {
+//       ...token,
+//       accessToken: refreshedTokens.access_token,
+//       accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
+//       refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+//     }
+//   } catch (error) {
+//     console.log(error)
+
+//     return {
+//       ...token,
+//       error: "RefreshAccessTokenError",
+//     }
+//   }
+// }
