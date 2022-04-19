d21260ec6174a26bf3065bed1a03173c510af953
Sebastian Bas <sbaskana@gmail.com>
2022-04-19 14:42:42 +0200

    add login error message and favicon

--- pages/login.js
+++ pages/login.js
@@ -8,24 +8,52 @@ import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
+import CircularProgress from '@mui/material/CircularProgress';
+import { green } from '@mui/material/colors';
 import useTranslation from 'next-translate/useTranslation'
 import { signIn, getSession } from "next-auth/react"
+import { useRouter } from 'next/router'
 
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export default function Login() {
   const { t, lang } = useTranslation('common')
+  const router = useRouter()
+  const {error} = router.query
 
+  const [loading, setLoading] = React.useState(false);
   // const [errorMsg, setErrorMsg] = useState('')
+  const initOpen = error !== undefined
+  // snackar alerts
+  const [open, setOpen] = useState(initOpen);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    if (!loading) {
+      setLoading(true);
+    }
+
     const data = new FormData(event.currentTarget);
     signIn("credentials", { username: data.get('email'), password: data.get('password') })
   };
 
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
   return (
+    <div>
     <Container maxWidth="sm">
       <Box
         sx={{
@@ -70,10 +98,23 @@ export default function Login() {
               type="submit"
               fullWidth
               variant="contained"
+              disabled={loading}
               sx={{ mt: 3, mb: 2 }}
           >
               {t("Log In")}
           </Button>
+          {loading && (
+            <Box sx={{ 
+              display: 'flex',
+              justifyContent: 'center', 
+              m: 1
+            }}>
+              <CircularProgress
+                size={36}
+                color='primary'
+              />
+            </Box>
+          )}
           {/* <Grid container>
               <Grid item xs>
               <Link href="#" variant="body2">
@@ -89,6 +130,16 @@ export default function Login() {
           </Box>
       </Box>
     </Container>
+    <Snackbar
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      open={open}
+      onClose={handleClose}
+    >
+      <Alert onClose={handleClose} severity="error">
+        {t("Problem Login")}
+      </Alert>
+    </Snackbar>
+    </div>
   );
 }
 
